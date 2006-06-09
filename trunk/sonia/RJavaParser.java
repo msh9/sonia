41f01e82b2feea1a490fa76958988cdb387c399c
skyebend <skyebend@fb298286-8e17-4df1-bf80-b634edc3145b>
2006-06-09 18:14:52 +0000

    empty class, but will be used to parse data passed directly from R to java
    git-svn-id: http://svn.code.sf.net/p/sonia/source_code@26 fb298286-8e17-4df1-bf80-b634edc3145b

---
+++ trunk/sonia/RJavaParser.java
@@ -0,0 +1,54 @@
+package sonia;
+
+import java.io.IOException;
+import java.util.Vector;
+
+/**
+ * Instead of reading a file as input, this class takes sets of arrays passed
+ * in via the rJava calling methods from R
+ * @author skyebend
+ *
+ */
+public class RJavaParser implements Parser {
+
+	public void parseNetwork(String fileAndPath) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getMaxNumNodes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getNumNodeEvents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getNumArcEvents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Vector getNodeList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vector getArcList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getNetInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getParserInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
