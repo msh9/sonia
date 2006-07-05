99d4a2f59a833a0febc6c917205f2c072e87028e
skyebend <skyebend@fb298286-8e17-4df1-bf80-b634edc3145b>
2006-07-05 21:44:06 +0000

    changed to swing components
    git-svn-id: http://svn.code.sf.net/p/sonia/source_code@40 fb298286-8e17-4df1-bf80-b634edc3145b

--- trunk/sonia/AttributeMapperDialog.java
+++ trunk/sonia/AttributeMapperDialog.java
@@ -7,6 +7,14 @@ package sonia;
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 /**
  *
  * @author skyebend
@@ -16,12 +24,14 @@ public class AttributeMapperDialog extends Object{
     private ArrayList unmapped;
     private ColumnMap map;
     private Dialog dialog;
+    private JScrollPane sp;
+    private JPanel layoutPanel;
     
     private ItemListener choiceListener =  new ItemListener() {
             public void itemStateChanged(ItemEvent e){
                 //debug
-                System.out.println(((Choice)e.getSource()).getName()+":"+ e.getItem());
-                map.setValueForFieldName(((Choice)e.getSource()).getName(), e.getItem().toString());
+                System.out.println(((JComboBox)e.getSource()).getName()+":"+ e.getItem());
+                map.setValueForFieldName(((JComboBox)e.getSource()).getName(), e.getItem().toString());
                 //need to put the deselected item on the list for others to use...
             }
         };
@@ -34,65 +44,76 @@ public class AttributeMapperDialog extends Object{
         this.unmapped = unmapped;
         this.map = map;
         //make the dialog
-        dialog = new Dialog(new Frame(),"Unrecognized column names in the input file",true);
+        dialog = new JDialog(new JFrame(),"Unrecognized column names in the input file",true);
         //make new font to help keep layouts consitant across platforms
         Font textFont = new Font("Monospaced ",Font.PLAIN,10);
-        dialog.setFont(textFont);
-        dialog.setLayout(new GridBagLayout());
+        layoutPanel = new JPanel();
+        layoutPanel.setFont(textFont);
+        layoutPanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.insets = new Insets(0,2,0,2);
         //make the components
-        Label explainLabel = new Label("Please choose appropriate column name assignments for each attribute:");
-        Label attrLabel = new Label("Network Attributes:");
-        Label colLabel = new Label("Column Names:");
-        Button ok = new Button("OK");
+        JLabel explainLabel = new JLabel("Please choose appropriate column name assignments for each attribute:");
+        JLabel attrLabel = new JLabel("Network Attributes:");
+        JLabel colLabel = new JLabel("Column Names:");
+        JButton ok = new JButton("OK");
         ok.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
-                dialog.hide();
+                dialog.setVisible(false);
                 dialog = null;
             }
         });
         c.gridx=0;c.gridy=0;c.gridwidth=2;c.gridheight=1;c.weightx=1;c.weighty=1.5;
-        dialog.add(explainLabel,c);
+        //layoutPanel.add(explainLabel,c);
         c.gridx=0;c.gridy=1;c.gridwidth=1;c.gridheight=1;c.weightx=1;c.weighty=1;
-        dialog.add(attrLabel,c);
+        layoutPanel.add(attrLabel,c);
         c.gridx=1;c.gridy=1;c.gridwidth=1;c.gridheight=1;c.weightx=1;c.weighty=1;
-        dialog.add(colLabel,c);
+        layoutPanel.add(colLabel,c);
         int rowNum = 2;
         ArrayList mapKeys =map.getMapKeys();
         Iterator attrKeyIter = mapKeys.iterator();
         //loop to add all the attribute labels
         while(attrKeyIter.hasNext()) {
             c.gridx=0;c.gridy=rowNum;c.gridwidth=1;c.gridheight=1;c.weightx=1;c.weighty=1;
-            c.anchor=c.EAST;
-            dialog.add(new Label((String)attrKeyIter.next()),c);
+            c.anchor=GridBagConstraints.EAST;
+            layoutPanel.add(new JLabel((String)attrKeyIter.next()),c);
             rowNum++;
         }
         c.gridx=0;c.gridy=rowNum;c.gridwidth=2;c.gridheight=1;c.weightx=1;c.weighty=1.5;
-        dialog.add(ok,c);
+        
         //loop to add the column choices
         ArrayList values = map.getMapValues();
         for (int i=0;i<values.size();i++) {
             //make a new list of choices
-            Choice choiceList = new Choice();
+        	unmapped.add(0,(String)values.get(i));
+            JComboBox choiceList = new JComboBox(unmapped.toArray());
             choiceList.setName((String)mapKeys.get(i));
             choiceList.addItemListener(choiceListener);
-            choiceList.add((String)values.get(i));
+            unmapped.remove(0);
             //add the items to the list
-            Iterator unmappedIter = unmapped.iterator();
-            while (unmappedIter.hasNext()) {
-                choiceList.add((String)unmappedIter.next());
-            }
+            //Iterator unmappedIter = unmapped.iterator();
+           // while (unmappedIter.hasNext()) {
+           //     choiceList.addItem((String)unmappedIter.next());
+           // }
             c.gridx=1;c.gridy=2+i;c.gridwidth=1;c.gridheight=1;c.weightx=1;c.weighty=1;
-            c.anchor=c.WEST;
-            dialog.add(choiceList,c);
+            c.anchor=GridBagConstraints.WEST;
+            layoutPanel.add(choiceList,c);
         }
-        
-        dialog.setBackground(Color.lightGray);
-        //dialog.setSize(200,300);
+        dialog.setLayout(new GridBagLayout());
+        sp= new JScrollPane(layoutPanel);
+        c.gridx=0;c.gridy=0;c.gridwidth=1;c.gridheight=1;c.weightx=0;c.weighty=0;c.fill=GridBagConstraints.NONE;
+        dialog.add(explainLabel,c);
+        c.gridx=0;c.gridy=1;c.gridwidth=1;c.gridheight=1;c.weightx=1;c.weighty=1;c.fill=GridBagConstraints.BOTH;
+        dialog.add(sp,c);
+        c.gridx=0;c.gridy=2;c.gridwidth=1;c.gridheight=1;c.weightx=0;c.weighty=0;
+        	c.fill=GridBagConstraints.NONE;c.anchor=GridBagConstraints.CENTER;
+        dialog.add(ok,c);
+        //dialog.setBackground(Color.lightGray);
+        dialog.setSize(400,300);
+   
         dialog.setLocation(100,100);
-        dialog.pack();
-        dialog.show();
+        //dialog.pack();
+        dialog.setVisible(true);
     }
     
     
