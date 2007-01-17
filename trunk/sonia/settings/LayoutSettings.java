49dc3d553e32704bb5673325662b2ff6dc3416de
skyebend <skyebend@fb298286-8e17-4df1-bf80-b634edc3145b>
2007-01-17 07:44:56 +0000

    added static strings for layout names etc
    git-svn-id: http://svn.code.sf.net/p/sonia/source_code@88 fb298286-8e17-4df1-bf80-b634edc3145b

--- trunk/sonia/settings/LayoutSettings.java
+++ trunk/sonia/settings/LayoutSettings.java
@@ -29,5 +29,25 @@ public class LayoutSettings extends PropertySettings {
 	public static final String LAYOUT_SETTINGS = "LAYOUT_SETTINGS";
 	
 	
+	//------------ values --
+	
+	public static final String COORD_ORIG =  "coordinates from original file";
+	public static final String CIRCULAR = "circular layout";
+	public static final String FR = "FR layout";
+	public static final String RAND_FR = "random FR layout";
+			
+	public static final String RUB_FR = "Rubber-Band FR Layout";
+	public static final String MULTI_KK = "MultiComp KK Layout";
+	public static final String PI = "Moody PI layout";
+	public static final String METRIC_MDS = "MetricMDS (SVD)?" ;
+
+	public static final String NO_ANIMATION = "none";
+	public static final String COSITE_ANIMATION = "cosine animation" ;
+
+	public static final String NUM_TIES =  "Number  of i->j ties";
+	public static final String AVG_TIES = "Avg of i->j ties";
+	public static final String SUM_TIES = "Sum  of i->j ties" ;
+	
+	
 	
 }
