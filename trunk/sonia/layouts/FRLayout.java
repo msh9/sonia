2e0a3be49d630652855f299a4f796a357a829ac0
skyebend <skyebend@fb298286-8e17-4df1-bf80-b634edc3145b>
2007-02-27 03:09:07 +0000

    fixed bug reading  opt dist param from settings
    git-svn-id: http://svn.code.sf.net/p/sonia/source_code@98 fb298286-8e17-4df1-bf80-b634edc3145b

--- trunk/sonia/layouts/FRLayout.java
+++ trunk/sonia/layouts/FRLayout.java
@@ -152,7 +152,7 @@ public class FRLayout implements NetLayout, Runnable
     engine = eng;
     schedule = new CoolingSchedule(8);
     //schedule.setMaxPasses(maxPasses);
-    maxPasses = Integer.parseInt(settings.getProperty(MAX_PASS));
+   // maxPasses = Integer.parseInt(settings.getProperty(MAX_PASS,maxPasses+""));
 	schedule.setMaxPasses(maxPasses);
     schedule.parseCtlValueString("(0,1.0) (3,0.64) (9,0.33) (17,0.21) (30,0.12) (52,0.08) (74,0.04) (100,0.0)");
     control.showFrame(schedule);
